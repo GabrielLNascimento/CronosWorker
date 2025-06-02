a29af17ab319e6298e07dd25bda22f759f008724
Gabriel De Liz <160193330+GabrielLNascimento@users.noreply.github.com>
2025-06-02 17:23:06 -0300

    feat: new css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import "./styles/theme.css"
+import "./styles/global.css"
+
+import Button from "./components/Button";
+
 function App() {
     console.log('Estou no console');
-    return <h1>Sejam bem vindos</h1>;
+    return (
+        <>
+            <h1>Ola pessoal</h1>
+            <Button />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import styles from './Button.module.css';
+
+function Button() {
+    return <button className={styles.fundoCor}>Clique em Mim!</button>;
+}
+
+export default Button;
